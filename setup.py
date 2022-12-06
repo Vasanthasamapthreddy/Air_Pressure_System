0574de8018e7cbc98e9513a1a3bd273e35a38169
Vasanthasamapthreddy <vasanthareddymara04@gmail.com>
2022-12-06 17:07:11 +0530

    updated requirmentsfile

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+def get_requirments():
+    pass
+
+
+
+
+
+
+setup(
+    name="sensor",
+    version= "0.0.1",
+    author="ineuron",
+    author_email="vasanthareddymara04@gmail.com",
+    packages= find_packages(),
+    install_requires = get_requirments(),
+)
